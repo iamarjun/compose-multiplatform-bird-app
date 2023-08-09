d2039755fc0576fbded71419a850e729a6b5c92d
iamarjun <arjunmanoj1995@gmail.com>
2023-08-09 21:49:37 +0530

    Switch to a Grid View

--- shared/src/commonMain/kotlin/presentation/BirdScreen.kt
+++ shared/src/commonMain/kotlin/presentation/BirdScreen.kt
@@ -2,11 +2,14 @@ package presentation
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
@@ -14,6 +17,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import io.kamel.image.KamelImage
 import io.kamel.image.asyncPainterResource
@@ -35,16 +39,18 @@ fun BirdScreen(viewModel: BirdViewModel) {
                 CircularProgressIndicator()
             }
         } else {
-            LazyColumn(
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(2),
                 modifier = modifier.fillMaxSize(),
-                verticalArrangement = Arrangement.spacedBy(16.dp),
-                horizontalAlignment = Alignment.CenterHorizontally
+                verticalArrangement = Arrangement.spacedBy(8.dp),
+                horizontalArrangement = Arrangement.spacedBy(8.dp)
             ) {
                 items(state.birdImages) {
                     KamelImage(
-                        modifier = modifier.fillMaxSize(),
+                        modifier = modifier.fillMaxWidth().aspectRatio(1.0f),
                         resource = asyncPainterResource("https://sebi.io/demo-image-api/${it.path}"),
                         contentDescription = null,
+                        contentScale = ContentScale.Crop,
                         onLoading = {
                             CircularProgressIndicator(
                                 modifier = modifier.size(16.dp),
